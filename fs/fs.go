54f1174c3fe30a19e9d4fcc63f413345d20264d0
Alan Moore <ukmoore@gmail.com>
2015-09-15 16:47:23 -0700

    Updated fs implementation (still not working)

--- fs/fs.go
+++ fs/fs.go
@@ -3,6 +3,7 @@ package fs
 import (
 	"bytes"
 	"encoding/gob"
+	"strings"
 )
 
 // A general filesystem
@@ -10,6 +11,7 @@ import (
 type RootFileSystem struct {
 	BlockHandler  BlockHandler
 	Configuration string
+	SuperBlock    SuperBlockNode
 }
 
 type BlockNode int
@@ -51,11 +53,45 @@ func (rfs *RootFileSystem) Format(bc int, bs int) {
 	Root := rfs.BlockHandler.SaveRawBlock(-1, rawBlock(rdn))
 	sb := SuperBlockNode{bc, bs, Root}
 	rfs.BlockHandler.SaveRawBlock(0, rawBlock(sb))
+	rfs.SuperBlock = sb
+}
+
+// Returns the BlockNode and whether it is a directory or not
+func (dn *DirectoryNode) findNode(paths []string, handler BlockHandler, createFileNode bool) FileNode {
+	if len(paths) == 1 {
+		// This should be looking in the Files section and create if not exist (depending on createFileNode)
+		nodeId, ok := dn.Files[paths[0]]
+		var fileNode FileNode
+		if !ok {
+			// Create FileNode, update the Files section, write this DirectoryNode back
+			// Write FileNode back
+			// Return that fileNode (blank really at the moment)
+		} else {
+			fileNode = getFileNode(handler.GetRawBlock(nodeId))
+		}
+		return fileNode
+	} else {
+		// This should look in the directories section and create a new directory node if that does not exist (depending on createFileNode)
+		// Then recurse with a subset of the paths
+		newDnId, ok := dn.Folders[paths[0]]
+		var newDn DirectoryNode
+		if !ok {
+			// Create new DirectoryNode, write that, update this DirectoryNode, write that
+		} else {
+			newDn = getDirectoryNode(handler.GetRawBlock(newDnId))
+		}
+
+		return newDn.findNode(paths[1:], handler, createFileNode)
+	}
 }
 
 func (rfs *RootFileSystem) WriteFile(fileName string, contents []byte) {
 	// Find record for this fileName from RootFileSystem
 	// After splitting on /
+	parts := strings.Split(fileName, "/")
+	rawRoot := rfs.BlockHandler.GetRawBlock(rfs.SuperBlock.RootDirectory)
+	dn := getDirectoryNode(rawRoot)
+	fn := dn.findNode(parts, rfs.BlockHandler, true)
 	// Create block nodes if non-existent
 	// Eventually get to the last one, and write the bytes to a new (or existing one, overwriting)
 	// there. (suitably slicing based on BlockSize)
@@ -77,4 +113,22 @@ func rawBlock(sb interface{}) []byte {
 		return buffer.Bytes()
 	}
 	return nil
+}
+
+func getDirectoryNode(contents []byte) DirectoryNode {
+	buffer := bytes.NewBuffer(contents)
+
+	dec := gob.NewDecoder(buffer)
+	var ret DirectoryNode
+	dec.Decode(&ret)
+	return ret
+}
+
+func getFileNode(contents []byte) FileNode {
+	buffer := bytes.NewBuffer(contents)
+
+	dec := gob.NewDecoder(buffer)
+	var ret FileNode
+	dec.Decode(&ret)
+	return ret
 }
